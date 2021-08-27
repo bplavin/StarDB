e2a1dfa41232ffeeac3dda16343747bcd023469d
Bogdan Plavin <bohdan.plavin@gmail.com>
2021-08-27 15:17:48 -0500

    Dynamic list of people and toggle fucntionality for the random planet

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,20 +1,52 @@
 import React, { Component } from "react";
 import './item-list.css';
+import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner/spinner";
 
 export default class ItemList extends Component {
+
+    swapiService = new SwapiService();
+
+    state = {
+        peopleList: null
+    };
+
+    componentDidMount() {
+        this.swapiService
+            .getAllPeople()
+            .then((peopleList) => {
+                this.setState ({
+                    peopleList
+                });
+            });
+    }
+
+    renderItems(arr) {
+        return arr.map(({id, name}) => {
+            return (
+                <li className='list-group-tem'
+                key={id}
+                onClick={() => this.props.onItemSelected(id)}>
+                {name}
+                </li>
+            );
+        });
+    }
+
     render() {
+
+        const { peopleList } = this.state;
+
+        if (!peopleList) {
+            return <Spinner />;
+        }
+
+        const items = this.renderItems(peopleList);
+
         return (
-            <ul className='item-list list-group'>
-                <li className='list-group-tem'>
-                    Luke Skywalker
-                </li>
-                <li className='list-group-tem'>
-                    Darth Vader
-                </li>
-                <li className='list-group-tem'>
-                    R2-D2
-                </li>
-            </ul>
+        <ul className="item-list list-group">
+            {items}
+        </ul>
         );
     };
 };
